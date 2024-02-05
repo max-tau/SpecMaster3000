325c49dbf613957ef14f947e4219ac48143cf5e8
Max Tauroza <maxtauroza@gmail.com>
2024-02-05 11:07:44 +0000

    Add CocktailFilter component and styling, add cors to backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const cocktailsRouter = require("./routes/cocktails");
 const ingredientsRouter = require("./routes/ingredients");
 const productsRouter = require("./routes/products");
@@ -6,6 +7,7 @@ const productsRouter = require("./routes/products");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/cocktails", cocktailsRouter);
 app.use("/ingredients", ingredientsRouter);
 app.use("/products", productsRouter);
